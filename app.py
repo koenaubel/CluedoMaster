a7e55b3839c4e1fcfb4c2b79a29f6ee6987336f4
Jochem <jochem.aubel@gmail.com>
2019-04-01 22:33:50 +0200

    player names in session cookie

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
+import os
 
 app = Flask(__name__)
-
+app.secret_key = os.urandom(16)
 
 @app.route('/', methods=('GET','POST'))
 def home():
     if request.method == 'POST':
-
+        players = list()
+        for player in request.form.getlist('player'):
+            if player != "":
+                players.append(player)
+        session['players'] = players
         return redirect(url_for('selectcards'))
     return render_template('home.html')
 
@@ -31,6 +36,7 @@ def cards():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
