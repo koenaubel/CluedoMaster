5ae79c073632dd9d0fc9538f264c0bc03c605cfb
Jochem <jochem.aubel@gmail.com>
2019-08-03 23:02:45 +0200

    add service worker for offline support

--- static/service-worker.js
+++ static/service-worker.js
@@ -6,7 +6,21 @@ var preLoad = function(){
   console.log("Installing web app");
   return caches.open("offline").then(function(cache) {
     console.log("caching index and important routes");
-    return cache.addAll(["/cards", "/turns", "/",  "templates/offline.html"]);
+    return cache.addAll([
+        '/static/bootstrap.min.css',
+        '/static/custom.css',
+        '/static/favicon.ico',
+        '/static/manifest.json',
+        '/static/apple-touch-icon.png',
+        "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js",
+        "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js",
+        "https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js",
+        "/static/service-worker.js",
+        "/cards",
+        "/turns",
+        "/select_cards",
+        "/"
+    ]);
   });
 };
 
